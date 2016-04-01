5431ecd3c2419d265232609e026fb38e0ffee5a4
danyrjoass <dany@dany-HP>
2016-04-01 18:34:41 -0400

    fix(Settings): Settings de acuerdo al ambiente
    Settings de development y de producción.

---
+++ agile/settings/development.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from .production import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'agile_desarrollo',
+        'USER': 'agile',
+        'PASSWORD': 'admin123',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+    },
+}
+
